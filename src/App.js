d84b4ed03fdbd1207f1d002c19dd2cc082b21e8c
shukeel <triggaillest@gmail.com>
2022-07-31 08:57:24 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Dashboard from "./components/admin/Dashboard";
+import Profile from "./components/admin/Profile";
 import MasterLayout from "./layouts/admin/MasterLayout";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/admin/dashboard" element={<MasterLayout />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/profile" element={<Profile />} />
         </Routes>
       </Router>
       {/* <MasterLayout /> */}
