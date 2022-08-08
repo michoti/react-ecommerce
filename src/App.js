b869eb6aa5320783ec0ad9032f041e18151af5f8
shukeel <triggaillest@gmail.com>
2022-08-08 11:34:34 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Register from "./components/frontend/auth/Register";
 import MasterLayout from "./layouts/admin/MasterLayout";
 import axios from "axios";
 
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
 axios.defaults.withCredentials = true;
+
 
 function App() {
   return (
--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import swal from 'sweetalert';
 
 const Register = () => {
     const [regInput, setReg] = useState({
         name:'',
         email:'',
-        password:''
+        password:'',
+        error_list: [],
     });
 
     const handleInput = (e) => {
@@ -23,8 +25,20 @@ const Register = () => {
         };
 
         axios.get('/sanctum/csrf-cookie').then(response => {
-            axios.post(`http://localhost:8000/api/register`, data)
-                 .then(res => {});
+            axios.post(`/api/register`, data)
+                 .then(res => {
+                    if(res.data.status === 200)
+                    {
+                        localStorage.setItem('auth_token', res.data.token);
+                        localStorage.setItem('auth_name', res.data.username);
+                        swal('success', res.data.message , 'success');
+
+                    }
+                    else
+                    {
+                        setReg({...regInput, error_list: res.data.validation_errors})
+                    }
+                 });
         });
 
         
@@ -37,14 +51,17 @@ const Register = () => {
                 <div className="mb-3">
                     <label className="form-label">Name</label>
                     <input type="text" name='name' onChange={handleInput} value={regInput.name} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <span>{regInput.error_list.name}</span>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Email address</label>
                     <input type="email" name='email' onChange={handleInput} value={regInput.email}  className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <span>{regInput.error_list.email}</span>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Password</label>
                     <input type="password" name='password' onChange={handleInput} value={regInput.password}  className="form-control" id="exampleInputPassword1" />
+                    <span>{regInput.error_list.password}</span>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
